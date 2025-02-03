a7c15702a31d758ef95857372fd59d5d20c9f629
S3465890 <S3465890@live.tees.ac.uk>
2025-02-03 15:46:09 +0000

    Changed name

--- app/src/main/java/scedt/myfirstapp/MainActivity.kt
+++ app/src/main/java/scedt/myfirstapp/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             MyFirstappTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Pavan",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
